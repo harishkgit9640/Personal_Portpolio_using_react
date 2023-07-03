0d35c1866c83f84099d592f9236f8bd0853fd0b1
Harish Kumar <harishstudio877@gmail.com>
2023-07-04 00:10:17 +0530

    navbar section has completed

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import './navStyle.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const hadleClick = () => { setClick(!click) };
     return (
         <div>
             <div className="navbar">
-                <NavLink to='/'> HK Solution </NavLink>
-                <ul>
+                <div className="brand">
+                    <NavLink to='/'> HK Solution </NavLink>
+                </div>
+
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
                     <li>
                         <NavLink to='/'> Home </NavLink>
                     </li>
@@ -19,6 +26,11 @@ const Navbar = () => {
                         <NavLink to='/contact'> Contact </NavLink>
                     </li>
                 </ul>
+                <div className="hamburger" onClick={hadleClick}>
+                    {
+                        click ? (<FaTimes size={20} style={{ color: "#fff" }} />) : (<FaBars size={20} style={{ color: "#fff" }} />)
+                    }
+                </div>
             </div>
         </div>
     )
